b36ed1a3b60bb9d8ae8448375849de84ddf7992a
mohamednajiub <m.emad550@gmail.com>
2019-11-04 20:47:20 +0200

    Feat: add product list interace

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {IProduct} from './product';
 
 @Component({
     selector: 'products',
@@ -7,7 +8,7 @@ import {Component} from '@angular/core';
 
 export class ProductListComponent {
     pageTitle: string = 'Product List';
-    products: any[] = [
+    products: IProduct[] = [
         {
           "productId": 1,
           "productName": "Leaf Rake",
