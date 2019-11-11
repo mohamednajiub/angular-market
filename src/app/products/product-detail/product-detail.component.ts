cc609dfe7b4bace8e5e5fef55fd5f2cc2cb937a0
mohamednajiub <m.emad550@gmail.com>
2019-11-11 14:54:05 +0200

    add routing to application

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute, Router} from '@angular/router';
 import {IProduct} from '../product';
 
 @Component({
@@ -10,9 +10,20 @@ export class ProductDetailComponent implements OnInit {
   
   productName: string;
   product: IProduct;
-  constructor() { }
+
+  constructor(private route: ActivatedRoute,
+              private router: Router) {
+    
+  }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+
+    console.log(id)
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products'])
   }
 
 }
